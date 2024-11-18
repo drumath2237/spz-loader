f5c55a90b44cfdd2dae37fb55011f9e865538989
drumath2237 <2237k.drumath@gmail.com>
2024-11-18 19:43:07 +0900

    :sparkles: babylon用のパッケージをコピーして作成

--- packages/babylonjs/vite.config.ts
+++ packages/babylonjs/vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({}) => {
     build: {
       lib: {
         entry: "./lib/index.ts",
-        name: "SpzWasmCore",
+        name: "SpzWasmBabylonjs",
         fileName: "index",
       },
     },
