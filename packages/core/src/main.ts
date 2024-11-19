a09b22acb482819a32b7d14ab21636aea2a2d0df
drumath <2237k.drumath@gmail.com>
2024-11-20 03:20:58 +0900

    :bento: add testbed spz asset

--- packages/core/src/main.ts
+++ packages/core/src/main.ts
@@ -1,11 +1,16 @@
 import "./style.css";
-import { dummy } from "../lib/";
+import { loadSpz } from "../lib/";
 
-const app = document.querySelector<HTMLDivElement>("#app");
-if (app) {
-  app.innerHTML = `
-  <div>
-    <p>${dummy()}</p>
-  </div>
-`;
-}
+import spzUrl from "../assets/guitarroom.spz?url";
+
+const main = async () => {
+  const spzDataBuffer = await fetch(spzUrl)
+    .then((res) => res.arrayBuffer())
+    .then((buf) => new Uint8Array(buf));
+
+  const gsCloud = await loadSpz(spzDataBuffer);
+
+  console.log(gsCloud);
+};
+
+main();
