973342d5c9684d9079f08db98394e7598963d1b0
drumath2237 <2237k.drumath@gmail.com>
2024-11-19 18:25:51 +0900

    :zap: remove unused cpp code

--- packages/core/lib/spz-wasm/main.cpp
+++ packages/core/lib/spz-wasm/main.cpp
@@ -15,35 +15,6 @@ using namespace std;
 using namespace spz;
 using namespace emscripten;
 
-float hoge()
-{
-  GaussianCloud gs = {
-      1, 0, false, {0.0, 0.0, 0.0}, {1.0, 1.0, 1.0}, {1.0, 0.0, 0.0, 0.0}, {1.0}, {1.0, 1.0, 1.0}, {}};
-
-  vector<uint8_t> binGS;
-  saveSpz(gs, &binGS);
-
-  PackedGaussians unpackedGS = loadSpzPacked(binGS);
-
-  return unpackedGS.numPoints;
-}
-
-GaussianCloud create_test(int points)
-{
-  GaussianCloud gs = {points};
-  return gs;
-}
-
-vector<uint8_t> save_spz(int points)
-{
-  GaussianCloud gs = {points};
-
-  vector<uint8_t> packed;
-  saveSpz(gs, &packed);
-
-  return packed;
-}
-
 GaussianCloud load_spz(const int gsPtr, const int length)
 {
   auto pointer = (uint8_t *)gsPtr;
@@ -59,9 +30,6 @@ int vf32_ptr(vector<float> &v)
 
 EMSCRIPTEN_BINDINGS(my_module)
 {
-  emscripten::function("create_test", &create_test);
-  emscripten::function("hoge", &hoge);
-  emscripten::function("save_spz", &save_spz);
   emscripten::function("load_spz", &load_spz, allow_raw_pointers());
   emscripten::function("vf32_ptr", &vf32_ptr, allow_raw_pointers());
 
