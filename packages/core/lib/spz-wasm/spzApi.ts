b83d2abdae329d0d2416c94d279395fe89664ba0
drumath2237 <2237k.drumath@gmail.com>
2025-04-11 19:23:54 +0900

    :sparkles: add loadFromUrl api to core

--- packages/core/lib/spz-wasm/spzApi.ts
+++ packages/core/lib/spz-wasm/spzApi.ts
@@ -5,7 +5,7 @@ import {
   disposeRawGSCloud,
 } from "./gaussianCloud";
 
-export interface ILoadSpzOptions {
+interface ILoadSpzOptions {
   colorScaleFactor?: number;
 }
 
@@ -14,23 +14,28 @@ export interface ILoadSpzOptions {
  * @param spzData .spz file binary data
  * @returns Gaussian Cloud
  */
-export const loadSpz = async (
-  spzData: Uint8Array,
+const loadSpz = async (
+  spzData: Uint8Array | ArrayBuffer,
   options?: ILoadSpzOptions,
 ): Promise<GaussianCloud> => {
   const wasmModule = await MainModuleFactory();
 
+  const spzBuffer =
+    spzData instanceof Uint8Array ? spzData : new Uint8Array(spzData);
+
   let pointer: number | null = null;
 
   try {
-    pointer = wasmModule._malloc(Uint8Array.BYTES_PER_ELEMENT * spzData.length);
+    pointer = wasmModule._malloc(
+      Uint8Array.BYTES_PER_ELEMENT * spzBuffer.length,
+    );
     if (pointer === null) {
       throw new Error("could'nt allocate memory");
     }
 
-    wasmModule.HEAPU8.set(spzData, pointer / Uint8Array.BYTES_PER_ELEMENT);
+    wasmModule.HEAPU8.set(spzBuffer, pointer / Uint8Array.BYTES_PER_ELEMENT);
 
-    const rawGsCloud = wasmModule.load_spz(pointer, spzData.length);
+    const rawGsCloud = wasmModule.load_spz(pointer, spzBuffer.length);
     const gaussianCloud = createGaussianCloudFromRaw(
       wasmModule,
       rawGsCloud,
@@ -47,3 +52,14 @@ export const loadSpz = async (
     }
   }
 };
+
+const loadSpzFromUrl = (
+  url: string,
+  options?: ILoadSpzOptions,
+): Promise<GaussianCloud> => {
+  return fetch(url)
+    .then((res) => res.arrayBuffer())
+    .then((data) => loadSpz(data, options));
+};
+
+export { type ILoadSpzOptions, loadSpz, loadSpzFromUrl };
