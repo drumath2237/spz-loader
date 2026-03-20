e848bab0cc6477d8c295cd2f31951cc93a3cfc19
Adam Morris <adam@cesium.com>
2026-03-20 12:22:34 -0500

    Adjusted floatVectorToFloatArray for faster copy and post-processing

--- packages/core/lib/spz-wasm/cppBufferUtil.ts
+++ packages/core/lib/spz-wasm/cppBufferUtil.ts
@@ -4,18 +4,25 @@ import type { MainModule, VectorFloat32, VectorUInt8T } from "./build/main";
  * create new Float32Array from cpp FloatVector
  * @param wasmModule emscripten main module
  * @param vec cpp float32 vector
+ * @param enhancementFunc function accepting a number and returning a number to perform processing
  * @returns copied float32 array
  */
 export const floatVectorToFloatArray = (
   wasmModule: MainModule,
   vec: VectorFloat32,
-  enhancementFunc: (n: number) => number = (n) => n,
+  enhancementFunc?: (n: number) => number,
 ): Float32Array => {
   const pointer = wasmModule.vf32_ptr(vec);
   const size = vec.size();
+  const floatOffset = pointer >> 2;
 
-  const buffer = new Float32Array(wasmModule.HEAPF32.buffer, pointer, size);
-  const copiedBuffer = buffer.map(enhancementFunc);
+  const copiedBuffer = wasmModule.HEAPF32.slice(floatOffset, floatOffset + size);
+
+  if (enhancementFunc !== undefined) {
+    for (let i = 0; i < size; i++) {
+      copiedBuffer[i] = enhancementFunc(copiedBuffer[i]);
+    }
+  }
 
   return copiedBuffer;
 };
